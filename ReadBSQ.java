bc5073b5bcf21e02ab3033fce5317c74be65cfe4
Dan Stutzman <dan@danstutzman.com>
2016-08-02 17:23:59 -0600

    Read .hdr to determine height and width.  Shrink size by one tenth.  Grayscale + alpha instead of shades of red.

--- ReadBSQ.java
+++ ReadBSQ.java
@@ -1,5 +1,8 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Transparency;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.ComponentColorModel;
@@ -10,55 +13,87 @@ import java.awt.image.WritableRaster;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.DoubleBuffer;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.File;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import javax.imageio.ImageIO;
 
 public class ReadBSQ {
   public static void main(String[] argv) throws java.io.IOException {
-    int width = 908;
-    int height = 11504;
+    if (argv.length < 1) {
+      System.err.println("First arg should be path to BSQ .dat file");
+      System.exit(1);
+    }
+    File path = new File(argv[0]);
 
-    String path = "NIS1_20140603_160543_atmcor_engL2VI.bsq";
-    double[] input = new double[width * height];
+    int inputWidth = 0;
+    int inputHeight = 0;
+    File hdrFile = new File(path.getAbsolutePath().replace(".dat", ".hdr"));
+    BufferedReader stream =
+      new BufferedReader(new InputStreamReader(new FileInputStream(hdrFile)));
+    while (true) {
+      String line = stream.readLine();
+      if (line == null) {
+        break;
+      }
+
+      if (line.startsWith("samples")) {
+        inputWidth = Integer.parseInt(line.split(" = ")[1]);
+      } else if (line.startsWith("lines")) {
+        inputHeight = Integer.parseInt(line.split(" = ")[1]);
+      }
+    }
+
+    double[] input = new double[inputWidth * inputHeight];
     FileInputStream is = new FileInputStream(path);
     byte bytes[] = new byte[1024];
     int read;
     int i = 0;
-    while((i < width * height - bytes.length) && (read = is.read(bytes)) != -1){
+    while((i < inputWidth * inputHeight - bytes.length) &&
+        (read = is.read(bytes)) != -1){
       DoubleBuffer doubles =
         ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN).asDoubleBuffer();
       doubles.get(input, i, read / 8);
       i += read / 8;
     }
 
-    byte[] aByteArray = new byte[width * height * 3];
+    byte[] aByteArray = new byte[inputWidth * inputHeight * 4];
     DataBuffer buffer = new DataBufferByte(aByteArray, aByteArray.length);
-    for (i = 0; i < width * height; i++) {
+    for (i = 0; i < inputWidth * inputHeight; i++) {
       double level = input[i];
       byte out;
       if (level <= 0.0) {
-        out = 0x00;
-      } else if (level >= 1.0) {
         out = (byte)0xff;
+      } else if (level >= 1.0) {
+        out = 0x00;
       } else {
-        out = (byte)(level * 255);
+        out = (byte)((1.0 - level) * 255);
       }
 
-      aByteArray[i * 3] = out;
+      aByteArray[i * 4 + 0] = out; // red
+      aByteArray[i * 4 + 1] = out; // green
+      aByteArray[i * 4 + 2] = out; // blue
+      aByteArray[i * 4 + 3] = (level > 0.0) ? (byte)255 : 0; // alpha
     }
 
     //3 bytes per pixel: red, green, blue
     WritableRaster raster = Raster.createInterleavedRaster(
-      buffer, width, height, 3 * width, 3, new int[] {0, 1, 2}, (Point)null);
+      buffer, inputWidth, inputHeight, 4 * inputWidth, 4, new int[] {0, 1, 2, 3},
+      (Point)null);
     ColorModel cm = new ComponentColorModel(
       ColorModel.getRGBdefault().getColorSpace(),
-      false, true, Transparency.OPAQUE, DataBuffer.TYPE_BYTE); 
+      true, false, Transparency.TRANSLUCENT, DataBuffer.TYPE_BYTE);
     BufferedImage image = new BufferedImage(cm, raster, true, null);
-    
-    ImageIO.write(image, "png", new File("image.png"));
+
+    BufferedImage out = new BufferedImage(
+      (inputWidth + 9) / 10, (inputHeight + 9) / 10, BufferedImage.TYPE_INT_ARGB);
+    Graphics2D g = out.createGraphics();
+    AffineTransform transform = AffineTransform.getScaleInstance(0.1, 0.1);
+    g.drawRenderedImage(image, transform);
+    ImageIO.write(out, "png", new File(path.getName() + ".png"));
   }
 }
--- read-bsq.sh
+++ read-bsq.sh
@@ -1,2 +1,6 @@
 #!/bin/bash -ex
-javac ReadBSQ.java && java -Djava.awt.headless=true ReadBSQ
+javac ReadBSQ.java
+for BSQ in /Volumes/AOP_1.3a_w_WF_v1.1a/1.3a/D10/CPER/2013/CPER_L2/CPER_Spectrometer/Veg_Indices/*.dat; do
+  java -Djava.awt.headless=true ReadBSQ $BSQ
+  echo $BSQ
+done
