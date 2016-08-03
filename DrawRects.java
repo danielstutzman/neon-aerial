737e5ecdbb5b3cc42c5195816fcc6d4f43133266
Dan Stutzman <dan@danstutzman.com>
2016-08-02 19:48:24 -0600

    Zoom in closer so map takes up almost entire image.

--- DrawRects.java
+++ DrawRects.java
@@ -17,8 +17,8 @@ public class DrawRects {
   }
 
   public void mainInstance(String[] argv) throws java.io.IOException {
-    //File[] files = new File("/Volumes/AOP_1.3a_w_WF_v1.1a/1.3a/D3/OSBS/2014/OSBS_L2/OSBS_Spectrometer").listFiles();
-    File[] files = new File("/Volumes/AOP_1.3a_w_WF_v1.1a/1.3a/D3/OSBS/2014/OSBS_L2/OSBS_Spectrometer/Veg_Indices").listFiles();
+    File[] files = new File("/Volumes/AOP_1.3a_w_WF_v1.1a/1.3a/D10/CPER/2013/CPER_L2/CPER_Spectrometer/Veg_Indices").listFiles();
+    //File[] files = new File("/Volumes/AOP_1.3a_w_WF_v1.1a/1.3a/D3/OSBS/2014/OSBS_L2/OSBS_Spectrometer/Veg_Indices").listFiles();
     List<Header> headers = new ArrayList<Header>();
     for (File file : files) {
       int inputWidth = 0;
@@ -59,23 +59,58 @@ public class DrawRects {
     float minEasting  = Float.POSITIVE_INFINITY;
     float maxEasting  = Float.NEGATIVE_INFINITY;
     for (Header header : headers) {
-      int maxHeightOrWidth = (header.inputHeight > header.inputWidth) ?
-        header.inputHeight : header.inputWidth;
-      float y0 = header.northing - maxHeightOrWidth;
-      float y1 = header.northing + maxHeightOrWidth;
-      float x0 = header.easting - maxHeightOrWidth;
-      float x1 = header.easting + maxHeightOrWidth;
-      if (x0 < minEasting) { minEasting = x0; }
-      if (x1 > maxEasting) { maxEasting = x1; }
-      if (y0 < minNorthing) { minNorthing = y0; }
-      if (y1 > maxNorthing) { maxNorthing = y1; }
+      float angle = (float)(header.rotation * Math.PI / 180);
+      float x0 = header.easting;
+      float y0 = header.northing;
+      XY corner1 = rotatePoint(header.easting + header.inputWidth,
+        header.northing,
+        x0, y0, angle);
+      XY corner2 = rotatePoint(header.easting + header.inputWidth,
+        header.northing - header.inputHeight,
+        x0, y0, angle);
+      XY corner3 = rotatePoint(header.easting,
+        header.northing - header.inputHeight,
+        x0, y0, angle);
+
+      if (x0 < minEasting)        { minEasting = x0; }
+      if (corner1.x < minEasting) { minEasting = corner1.x; }
+      if (corner2.x < minEasting) { minEasting = corner2.x; }
+      if (corner3.x < minEasting) { minEasting = corner3.x; }
+
+      if (x0 > maxEasting)        { maxEasting = x0; }
+      if (corner1.x > maxEasting) { maxEasting = corner1.x; }
+      if (corner2.x > maxEasting) { maxEasting = corner2.x; }
+      if (corner3.x > maxEasting) { maxEasting = corner3.x; }
+
+      if (y0 < minNorthing)        { minNorthing = y0; }
+      if (corner1.y < minNorthing) { minNorthing = corner1.y; }
+      if (corner2.y < minNorthing) { minNorthing = corner2.y; }
+      if (corner3.y < minNorthing) { minNorthing = corner3.y; }
+
+      if (y0 > maxNorthing)        { maxNorthing = y0; }
+      if (corner1.y > maxNorthing) { maxNorthing = corner1.y; }
+      if (corner2.y > maxNorthing) { maxNorthing = corner2.y; }
+      if (corner3.y > maxNorthing) { maxNorthing = corner3.y; }
     }
+
+    // Add a small margin
+    minEasting -= 100;
+    maxEasting += 100;
+    minNorthing -= 100;
+    maxNorthing += 100;
+
+    System.out.println("minEasting: " + minEasting);
+    System.out.println("maxEasting: " + maxEasting);
+    System.out.println("minNorthing: " + minNorthing);
+    System.out.println("maxNorthing: " + maxNorthing);
 
     int outWidth = 4000;
     int outHeight = 4000;
     BufferedImage img =
       new BufferedImage(outWidth, outHeight, BufferedImage.TYPE_INT_ARGB);
     Graphics2D g2d = img.createGraphics();
+    g2d.scale(outWidth / (maxEasting - minEasting),
+      outHeight / (maxNorthing - minNorthing));
     for (Header header : headers) {
       File pngFilename = new File(header.filename.replace(".hdr", ".dat.png"));
       System.err.println("Reading " + pngFilename);
@@ -84,12 +119,10 @@ public class DrawRects {
       System.out.println(header.easting + " " + header.northing + " " +
         header.inputWidth + " " + header.inputHeight);
       g2d.setColor(Color.RED);
-      float northwestX =
-        (header.easting - minEasting) / (maxEasting - minEasting) * outWidth;
-      float northwestY =
-        (maxNorthing - header.northing) / (maxNorthing - minNorthing) * outHeight;
-      float width = header.inputWidth / (maxEasting - minEasting) * outWidth;
-      float height = header.inputHeight / (maxNorthing - minNorthing) * outHeight;
+      float northwestX = (header.easting - minEasting);
+      float northwestY = (maxNorthing - header.northing);
+      float width = header.inputWidth;
+      float height = header.inputHeight;
       g2d.translate(northwestX, northwestY);
       g2d.rotate(-Math.toRadians(header.rotation));
       AffineTransform transform = AffineTransform.getScaleInstance(
@@ -114,5 +147,23 @@ public class DrawRects {
     float northing;
     float easting;
     float rotation;
+    public String toString() {
+      return filename;
+    }
+  }
+
+  class XY {
+    float x;
+    float y;
+  }
+
+  private XY rotatePoint(float inX, float inY, float centerX, float centerY,
+      float angle) {
+    XY xy = new XY();
+    xy.x = (float)(centerX + (inX - centerX) * Math.cos(angle) -
+      (inY - centerY) * Math.sin(angle));
+    xy.y = (float)(centerY + (inX - centerX) * Math.sin(angle) +
+      (inY - centerY) * Math.cos(angle));
+    return xy;
   }
 }
